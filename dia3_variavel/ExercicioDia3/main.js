c385a531ef8de6e3ec0e9abc9dce5d3e377f229a
Carlos Miguel Bissoni Ribeiro <bissoniribeirocarlosmiguel@gmail.com>
2023-08-21 21:47:11 -0300

    Update dia 3

---
+++ dia3_variavel/ExercicioDia3/main.js
@@ -0,0 +1,9 @@
+ let Nome = prompt('Digite sua Nome');
+ let idade = prompt('Digite sua idade');
+ let altura = prompt("digite sua altura");
+ let peso = prompt('digite seu peso');
+
+let Resultidade= 2023-idade;
+let IMC =  peso/(altura*altura);
+
+ console.log("Olá   "   + Nome+ "   você tem    "+ idade   + " nasceu no ano de : "   +Resultidade+   "   Tem    A altura  " + altura  +"   a altura Pesa  "    +   peso    +"     Seu imc é    "  +   IMC);
---
+++ dia3_variavel/main.js
@@ -0,0 +1,26 @@
+// // entendendo os tipos:
+// let Tipotexto = "2";
+// let TipoNumber = 3;
+// let TipoBool = true;
+// let tipotext="true"
+// console.log(Tipotexto,TipoNumber,TipoBool,tipotext);
+// let meuNome = 'Carlos Miguel Bissoni Ribeiro '
+// console.log(meuNome);
+// meuNome = 'Carlos M'
+// console.log('my name is :'+ meuNome);
+// // vendo na pratica a diferenca de let e const. 
+// console.log('####################### const###################3')
+// const myName = 'Carlos Miguel Bissoni Ribeiro '
+// console.log(myName);
+// // myName = 'Carlos M'
+// // console.log('my name is :'+ myName);
+// // somar *************************************
+
+// let number1 = 100000;
+// let number2 = 300000;
+// let Result = number1+number2;
+// console.log("o valor da soma é "+ Result);
+
+// Receber Dados do usuario:
+ let entradaDoUsuario = prompt();
+ console.log(entradaDoUsuario)
